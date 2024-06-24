66bf367e0fca6caf450e6a9466e237ec2b7b4eb7
hoanghd <haduchoang@cryptopie-labo.com>
2024-06-24 22:43:57 +0700

    adding keys to generatekey func

--- DuckeyBoard/Helpers/Keyboard.cs
+++ DuckeyBoard/Helpers/Keyboard.cs
@@ -1,5 +1,5 @@
 ﻿namespace DuckeyBoard.Helpers
-{   
+{
     public class Keyboard
     {
         public List<KeyboardKey> KeyboardKeys { get; set; }
@@ -9,10 +9,132 @@
             KeyboardKeys = GenerateKeys();
         }
 
-        public List<KeyboardKey> GenerateKeys() 
+        public List<KeyboardKey> GenerateKeys()
         {
             List<KeyboardKey> result = new List<KeyboardKey>();
-            result.Add(new ()
+
+            #region R1F
+            result.Add(new()
+            {
+                Title = "ESC",
+                ExtraTitle = "",
+                Unit = 1,
+                Row = KeyboardRow.R1F,
+                RowIndex = 0,
+                IndexInRow = 0
+            });
+            result.Add(new()
+            {
+                Title = "F1",
+                ExtraTitle = "",
+                Unit = 1,
+                Row = KeyboardRow.R1F,
+                RowIndex = 0,
+                IndexInRow = 0
+            });
+            result.Add(new()
+            {
+                Title = "F2",
+                ExtraTitle = "",
+                Unit = 1,
+                Row = KeyboardRow.R1F,
+                RowIndex = 0,
+                IndexInRow = 0
+            });
+            result.Add(new()
+            {
+                Title = "F3",
+                ExtraTitle = "",
+                Unit = 1,
+                Row = KeyboardRow.R1F,
+                RowIndex = 0,
+                IndexInRow = 0
+            });
+            result.Add(new()
+            {
+                Title = "F4",
+                ExtraTitle = "",
+                Unit = 1,
+                Row = KeyboardRow.R1F,
+                RowIndex = 0,
+                IndexInRow = 0
+            });
+            result.Add(new()
+            {
+                Title = "F5",
+                ExtraTitle = "",
+                Unit = 1,
+                Row = KeyboardRow.R1F,
+                RowIndex = 0,
+                IndexInRow = 0
+            });
+            result.Add(new()
+            {
+                Title = "F6",
+                ExtraTitle = "",
+                Unit = 1,
+                Row = KeyboardRow.R1F,
+                RowIndex = 0,
+                IndexInRow = 0
+            });
+            result.Add(new()
+            {
+                Title = "F7",
+                ExtraTitle = "",
+                Unit = 1,
+                Row = KeyboardRow.R1F,
+                RowIndex = 0,
+                IndexInRow = 0
+            });
+            result.Add(new()
+            {
+                Title = "F8",
+                ExtraTitle = "",
+                Unit = 1,
+                Row = KeyboardRow.R1F,
+                RowIndex = 0,
+                IndexInRow = 0
+            });
+            result.Add(new()
+            {
+                Title = "F9",
+                ExtraTitle = "",
+                Unit = 1,
+                Row = KeyboardRow.R1F,
+                RowIndex = 0,
+                IndexInRow = 0
+            });
+            result.Add(new()
+            {
+                Title = "F10",
+                ExtraTitle = "",
+                Unit = 1,
+                Row = KeyboardRow.R1F,
+                RowIndex = 0,
+                IndexInRow = 0
+            });
+            result.Add(new()
+            {
+                Title = "F11",
+                ExtraTitle = "",
+                Unit = 1,
+                Row = KeyboardRow.R1F,
+                RowIndex = 0,
+                IndexInRow = 0
+            });
+            result.Add(new()
+            {
+                Title = "12",
+                ExtraTitle = "",
+                Unit = 1,
+                Row = KeyboardRow.R1F,
+                RowIndex = 0,
+                IndexInRow = 0
+            });
+            #endregion
+
+            #region R1
+            result.Add(new()
             {
                 Title = "`",
                 ExtraTitle = "~",
@@ -39,7 +161,106 @@
                 RowIndex = 1,
                 IndexInRow = 0
             });
-
+            result.Add(new()
+            {
+                Title = "3",
+                ExtraTitle = "#",
+                Unit = 1,
+                Row = KeyboardRow.R1,
+                RowIndex = 1,
+                IndexInRow = 0
+            });
+            result.Add(new()
+            {
+                Title = "4",
+                ExtraTitle = "$",
+                Unit = 1,
+                Row = KeyboardRow.R1,
+                RowIndex = 1,
+                IndexInRow = 0
+            });
+            result.Add(new()
+            {
+                Title = "5",
+                ExtraTitle = "%",
+                Unit = 1,
+                Row = KeyboardRow.R1,
+                RowIndex = 1,
+                IndexInRow = 0
+            });
+            result.Add(new()
+            {
+                Title = "6",
+                ExtraTitle = "^",
+                Unit = 1,
+                Row = KeyboardRow.R1,
+                RowIndex = 1,
+                IndexInRow = 0
+            });
+            result.Add(new()
+            {
+                Title = "7",
+                ExtraTitle = "&",
+                Unit = 1,
+                Row = KeyboardRow.R1,
+                RowIndex = 1,
+                IndexInRow = 0
+            });
+            result.Add(new()
+            {
+                Title = "8",
+                ExtraTitle = "*",
+                Unit = 1,
+                Row = KeyboardRow.R1,
+                RowIndex = 1,
+                IndexInRow = 0
+            });
+            result.Add(new()
+            {
+                Title = "9",
+                ExtraTitle = "(",
+                Unit = 1,
+                Row = KeyboardRow.R1,
+                RowIndex = 1,
+                IndexInRow = 0
+            });
+            result.Add(new()
+            {
+                Title = "0",
+                ExtraTitle = ")",
+                Unit = 1,
+                Row = KeyboardRow.R1,
+                RowIndex = 1,
+                IndexInRow = 0
+            });
+            result.Add(new()
+            {
+                Title = "-",
+                ExtraTitle = "_",
+                Unit = 1,
+                Row = KeyboardRow.R1,
+                RowIndex = 1,
+                IndexInRow = 0
+            });
+            result.Add(new()
+            {
+                Title = "=",
+                ExtraTitle = "+",
+                Unit = 1,
+                Row = KeyboardRow.R1,
+                RowIndex = 1,
+                IndexInRow = 0
+            });
+            result.Add(new()
+            {
+                Title = "backspace",
+                ExtraTitle = "",
+                Unit = 1,
+                Row = KeyboardRow.R1,
+                RowIndex = 1,
+                IndexInRow = 0
+            });
+            #endregion
 
             return result;
         }
